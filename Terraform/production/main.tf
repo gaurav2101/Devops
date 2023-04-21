a7c1e91961439efe4d8b3e3a6c32b247642d57b3
Gaurav Yadav <gauravyadav2101@gmail.com>
2023-04-21 19:07:45 +0530

    Update main.tf

--- Terraform/production/main.tf
+++ Terraform/production/main.tf
@@ -1,1 +1,6 @@
-
+module "network" {
+    source = "../modules/network"
+    cluster_name           = "webservers-stage"
+    db_remote_state_bucket = "(YOUR_BUCKET_NAME)"
+    db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+}
